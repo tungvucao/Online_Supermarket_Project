028f2be10fb2f4c15a40128a66b33abd10f2ff15
VuCaoTung-Windown <tungvucao5197@gmail.com>
2024-04-08 20:37:20 +0700

    up date master

--- Online_Supermarket_Project/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Online_Supermarket_Project.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
