0f6b9008d2574d4d0b3518b25225bc15fef74f91
linhbong2015 <linhbong2015@gmail.com>
2024-04-24 02:09:35 +0700

    Thêm các sản phẩm vào trang và phân trang

--- Online_Supermarket_Project/Controllers/HomeController.cs
+++ Online_Supermarket_Project/Controllers/HomeController.cs
@@ -1,19 +1,30 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Online_Supermarket_Project.AppContext;
+using Online_Supermarket_Project.Models;
+using X.PagedList;
 
 namespace Online_Supermarket_Project.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly MyAppDbContext _db;
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(MyAppDbContext db, ILogger<HomeController> logger)
         {
+            _db = db;
             _logger = logger;
         }
 
-        public IActionResult Index()
+        public IActionResult Index(int? page)
         {
-            return View();
+            int pageSize = 8;
+            int pageNumber=page==null||page<0?1:page.Value;
+            var lstsanpham = _db.Product.AsNoTracking().OrderBy(x=>x.ProductName);
+            PagedList<Product> lst = new PagedList<Product>(lstsanpham, pageNumber, pageSize);
+
+            return View(lst);
         }
 
         public IActionResult Privacy()
