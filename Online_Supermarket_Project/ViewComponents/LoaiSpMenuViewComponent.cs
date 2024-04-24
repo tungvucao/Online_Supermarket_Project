c6ab92a03e605205775e97b09fb33f59fa4b3642
linhbong2015 <linhbong2015@gmail.com>
2024-04-24 18:43:58 +0700

    Tạo dynamic menu bằng Component

---
+++ Online_Supermarket_Project/ViewComponents/LoaiSpMenuViewComponent.cs
@@ -0,0 +1,19 @@
+﻿using Online_Supermarket_Project.Models;
+using Microsoft.AspNetCore.Mvc;
+using Online_Supermarket_Project.Repository;
+namespace Online_Supermarket_Project.ViewComponents
+{
+    public class LoaiSpMenuViewComponent : ViewComponent
+    {
+        private readonly ILoaiSpRepository _loaiSp;
+        public LoaiSpMenuViewComponent(ILoaiSpRepository loaiSpRepository)
+        {
+            _loaiSp = loaiSpRepository;
+        }
+        public IViewComponentResult Invoke()
+        {
+            var loaisp = _loaiSp.GetAllLoaiSp().OrderBy(x => x.CateName);
+            return View(loaisp);
+        }
+    }
+}
