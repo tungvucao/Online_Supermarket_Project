6a0c45214de3590837b02a630e971ae1ecb4ecb0
linhbong2015 <linhbong2015@gmail.com>
2024-04-25 19:42:21 +0700

    sanpham

---
+++ Online_Supermarket_Project/Controllers/ProductsController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Online_Supermarket_Project.AppContext;
+using Online_Supermarket_Project.Models;
+using X.PagedList;
+
+namespace Online_Supermarket_Project.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly MyAppDbContext _db;
+        private readonly ILogger<ProductsController> _logger;
+        public ProductsController(MyAppDbContext db, ILogger<ProductsController> logger)
+        {
+            _db = db;
+            _logger = logger;
+        }
+        [HttpGet("/sanpham")]
+        public IActionResult Index(int? page)
+        {
+            int pageSize = 8;
+            int pageNumber = page == null || page < 0 ? 1 : page.Value;
+            var lstsanpham = _db.Product.AsNoTracking().OrderBy(x => x.ProductName);
+            PagedList<Product> lst = new PagedList<Product>(lstsanpham, pageNumber, pageSize);
+
+            return View(lst);
+        }
+    }
+}
