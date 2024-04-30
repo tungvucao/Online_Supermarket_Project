f5445b64e9b8a68f1b72480725a03b7232f21ea1
linhbong2015 <linhbong2015@gmail.com>
2024-04-30 09:22:47 +0700

    trangtintuc

---
+++ Online_Supermarket_Project/Controllers/NewsController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Online_Supermarket_Project.AppContext;
+using Online_Supermarket_Project.Models;
+using X.PagedList; // Thêm namespace này
+using System.Linq;
+
+namespace Online_Supermarket_Project.Controllers
+{
+    public class NewsController : Controller
+    {
+        private readonly MyAppDbContext _db;
+        private readonly ILogger<NewsController> _logger;
+        public NewsController(MyAppDbContext db, ILogger<NewsController> logger)
+        {
+            _db = db;
+            _logger = logger;
+        }
+        [HttpGet("/News")]
+        public IActionResult Index(int? page)
+        {
+            int pageSize = 6; // Số lượng sản phẩm trên mỗi trang
+            int pageNumber = page ?? 1; // Trang hiện tại, mặc định là trang 1 nếu không có giá trị page
+
+            var lstnews = _db.New.AsNoTracking().OrderBy(x => x.Title);
+            var pagedNews = lstnews.ToPagedList(pageNumber, pageSize); // Sử dụng ToPagedList() để tạo IPagedList<New>
+
+            return View(pagedNews); // Trả về IPagedList<New> thay vì StaticPagedList<New>
+        }
+    }
+}
---
+++ Online_Supermarket_Project/ViewComponents/BaiBaoHotViewComponent.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Online_Supermarket_Project.AppContext;
+using Online_Supermarket_Project.Models;
+
+namespace Online_Supermarket_Project.ViewComponents
+{
+    public class BaiBaoHotViewComponent : ViewComponent
+    {
+        private readonly MyAppDbContext _db;
+        private readonly List<New> news;
+        public BaiBaoHotViewComponent(MyAppDbContext db)
+        {
+            _db = db;
+        }
+        public IViewComponentResult Invoke()
+        {
+            var newestNews = _db.New.AsNoTracking()
+                                        .OrderByDescending(x => x.IsHot)
+                                        .Take(3)
+                                        .ToList();
+            return View("Default", newestNews);
+        }
+    }
+}
